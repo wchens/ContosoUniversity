8aefc8f72c5c0c35d57c2bcf2315a00d4262d88b
Wong Chen Shin <wong.chen.shin@nccs.com.sg>
2016-08-30 18:03:57 +0800

    till pag 140

---
+++ ContosoUniversity/Migrations/201608300658310_InitialCreate1.cs
@@ -0,0 +1,18 @@
+namespace ContosoUniversity.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate1 : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Student", name: "FirstMidName", newName: "Firstname");
+        }
+        
+        public override void Down()
+        {
+            RenameColumn(table: "dbo.Student", name: "Firstname", newName: "FirstMidName");
+        }
+    }
+}
