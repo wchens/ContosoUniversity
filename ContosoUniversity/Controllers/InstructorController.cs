11071e72276515d6a417db9400e30f215c349a8e
Wong Chen Shin <wong.chen.shin@nccs.com.sg>
2016-09-01 16:02:26 +0800

    completed till Pg 204

--- ContosoUniversity/Controllers/InstructorController.cs
+++ ContosoUniversity/Controllers/InstructorController.cs
@@ -100,9 +100,10 @@ namespace ContosoUniversity.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Instructor instructor = db.Instructors
-                                    .Include(i => i.OfficeAssignment)
-                                    .Where(i => i.ID == id)
-                                    .Single();
+            .Include(i => i.OfficeAssignment).Include(i => i.Courses)
+            .Where(i => i.ID == id)
+            .Single(); 
+            PopulateAssignedCourseData(instructor);
             if (instructor == null)
             {
                 return HttpNotFound();
@@ -110,21 +111,38 @@ namespace ContosoUniversity.Controllers
             return View(instructor);
         }
 
+        private void PopulateAssignedCourseData(Instructor instructor) 
+        { 
+            var allCourses = db.Courses; 
+            var instructorCourses = new HashSet<int>(instructor.Courses.Select(c => c.CourseID)); 
+            var viewModel = new List<AssignedCourseData>(); 
+            foreach (var course in allCourses) 
+            { 
+                viewModel.Add(new AssignedCourseData 
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            } ViewBag.Courses = viewModel;
+        }
+
         // POST: Instructors/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost, ActionName("Edit")]
-        [ValidateAntiForgeryToken]
-        public ActionResult EditPost(int? id)
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public ActionResult Edit(int? id, string[] selectedCourses)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             var instructorToUpdate = db.Instructors
-            .Include(i => i.OfficeAssignment)
+            .Include(i => i.OfficeAssignment) .Include(i => i.Courses)
             .Where(i => i.ID == id)
             .Single();
+            
             if (TryUpdateModel(instructorToUpdate, "",
             new string[] { "LastName", "FirstMidName", "HireDate", "OfficeAssignment" }))
             {
@@ -134,6 +152,7 @@ namespace ContosoUniversity.Controllers
                     {
                         instructorToUpdate.OfficeAssignment = null;
                     }
+                    UpdateInstructorCourses(selectedCourses, instructorToUpdate);
                     db.Entry(instructorToUpdate).State = EntityState.Modified;
                     db.SaveChanges();
                     return RedirectToAction("Index");
@@ -143,9 +162,39 @@ namespace ContosoUniversity.Controllers
                     //Log the error (uncomment dex variable name and add a line here to write a log.
                     ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
                 }
-            }
+            } PopulateAssignedCourseData(instructorToUpdate);
             return View(instructorToUpdate);
         }
+
+
+        private void UpdateInstructorCourses(string[] selectedCourses, Instructor instructorToUpdate) 
+        { 
+            if (selectedCourses == null) 
+            { 
+                instructorToUpdate.Courses = new List<Course>(); 
+                return; 
+            } 
+            
+            var selectedCoursesHS = new HashSet<string>(selectedCourses); 
+            var instructorCourses = new HashSet<int>(instructorToUpdate.Courses.Select(c => c.CourseID)); 
+            foreach (var course in db.Courses) 
+            { 
+                if (selectedCoursesHS.Contains(course.CourseID.ToString())) 
+                { 
+                    if (!instructorCourses.Contains(course.CourseID)) 
+                    { 
+                        instructorToUpdate.Courses.Add(course); 
+                    } }
+                else 
+                { 
+                    if (instructorCourses.Contains(course.CourseID)) 
+                    { 
+                        instructorToUpdate.Courses.Remove(course); 
+                    } 
+                } 
+            } 
+        }
+
 
         // GET: Instructors/Delete/5
         public ActionResult Delete(int? id)
